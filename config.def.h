1fda54ac1e50b6f4b177e37eef277221a3cdc446
Elvis Wang <mail@wbprime.me>
2016-08-22 21:18:44 +0800

    Add prompt line in dmenu

--- config.def.h
+++ config.def.h
@@ -65,7 +65,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", monofonts, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-p", "Launch:", "-m", dmenumon, "-fn", monofonts, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", "-f", monofonts, "-e", "/usr/bin/fish", NULL };
 static const char *slockcmd[]  = { "slock", NULL };
 static const char *operacmd[]  = { "opera", "--proxy-pac-url=http://cross-gwf.58ganji-corp.com", NULL };
