df959d0acf5ae07f22e36d7abf3d04e0005dd240
Elvis Wang <mail@wbprime.me>
2016-08-25 14:48:03 +0800

    Add vim window rule

--- config.def.h
+++ config.def.h
@@ -29,15 +29,16 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   isterminal noswallow monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           0,         0,        -1 },
-	{ "VBoxSDL"           , NULL , NULL , 1 << 5 ,       0 ,   0, 0,       -1 } ,
-	{ "VirtualBox"        , NULL , NULL , 1 << 5 ,       0 ,   0, 0,       -1 } ,
-	{ "rdesktop"          , NULL , NULL , 1 << 5 ,       0 ,   0, 0,       -1 } ,
-	{ "Opera"             , NULL , NULL , 1 << 2 ,       0 ,   0, 0,       -1 } ,
-	{ "jetbrains-idea-ce" , NULL , NULL , 1 << 1 ,       0 ,   0, 0,       -1 } ,
-	{ "Surf"              , NULL , NULL , 1 << 3 ,       0 ,   0, 0,       -1 } ,
-	{ "Firefox"           , NULL , NULL , 1      ,       0 ,   0, 0,       -1 } ,
-	{ "st",       NULL,       NULL,       0,            0,           1,         1,        -1 },
+	{ "Gimp"              , NULL , NULL , 0      , 1 , 0 , 0 , -1 } ,
+	{ "VBoxSDL"           , NULL , NULL , 1 << 5 , 0 , 0 , 0 , -1 } ,
+	{ "VirtualBox"        , NULL , NULL , 1 << 5 , 0 , 0 , 0 , -1 } ,
+	{ "rdesktop"          , NULL , NULL , 1 << 5 , 0 , 0 , 0 , -1 } ,
+	{ "Opera"             , NULL , NULL , 1 << 2 , 0 , 0 , 0 , -1 } ,
+	{ "jetbrains-idea-ce" , NULL , NULL , 1 << 1 , 0 , 0 , 0 , -1 } ,
+	{ "Surf"              , NULL , NULL , 1 << 3 , 0 , 0 , 0 , -1 } ,
+	{ "Firefox"           , NULL , NULL , 1      , 0 , 0 , 0 , -1 } ,
+	{ "st"                , NULL , NULL , 0      , 0 , 1 , 1 , -1 } ,
+	{ "vim"               , NULL , NULL , 1 << 3 , 0 , 0 , 0 , -1 } ,
 };
 
 /* layout(s) */
