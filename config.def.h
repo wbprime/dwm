d3e4cf002d1e90239c84b50c76ca7ded17b9b0e7
Elvis Wang <mail@wbprime.me>
2016-08-29 11:25:08 +0800

    Add rule for vim

--- config.def.h
+++ config.def.h
@@ -38,7 +38,7 @@ static const Rule rules[] = {
 	{ "Surf"              , NULL , NULL , 1 << 3 , 0 , 0 , 0 , -1 } ,
 	{ "Firefox"           , NULL , NULL , 1      , 0 , 0 , 0 , -1 } ,
 	{ "st"                , NULL , NULL , 0      , 0 , 1 , 1 , -1 } ,
-	{ "vim"               , NULL , NULL , 1 << 3 , 0 , 0 , 0 , -1 } ,
+	{ NULL                , NULL , "vim", 1 << 3 , 0 , 0 , 0 , -1 } ,
 };
 
 /* layout(s) */
