4ced387aa4df44a56bdae40695d8face5b62788a
Elvis Wang <wangbo12@58ganji.com>
2016-12-29 09:15:02 +0800

    Change default sh from bash to zsh

--- config.def.h
+++ config.def.h
@@ -36,6 +36,7 @@ static const Rule rules[] = {
 	{ "rdesktop"           , NULL , NULL  , 1 << 5 , 0 , -1 } ,
 	{ "Opera"              , NULL , NULL  , 1 << 2 , 0 , -1 } ,
 	{ "vivaldi-stable"     , NULL , NULL  , 1 << 2 , 0 , -1 } ,
+	{ "Vivaldi-stable"     , NULL , NULL  , 1 << 2 , 0 , -1 } ,
 	{ "jetbrains-idea-ce"  , NULL , NULL  , 1 << 1 , 0 , -1 } ,
 	{ "Surf"               , NULL , NULL  , 1 << 3 , 0 , -1 } ,
 	{ "Firefox"            , NULL , NULL  , 1      , 0 , -1 } ,
@@ -70,7 +71,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-p", "Launch:", "-m", dmenumon, "-fn", monofonts, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", "-f", monofonts, "-e", "/bin/bash", NULL };
+static const char *termcmd[]  = { "st", "-f", monofonts, "-e", "/bin/zsh", NULL };
 static const char *slockcmd[]  = { "slock", NULL };
 static const char *operacmd[]  = { "vivaldi", "--proxy-pac-url=http://cross-gwf.58ganji-corp.com", NULL };
 
